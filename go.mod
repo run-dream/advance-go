4a9802f983b30338a4ce28a00d32e5d76b8eee38
ruanjun <837778355@qq.com>
2021-08-05 13:38:39 +0800

    wire

--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module go-advance
 go 1.16
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/fatih/color v1.12.0 // indirect
+	github.com/go-kratos/kratos v1.0.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/google/wire v0.5.0 // indirect
+	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 )
---
+++ src/wire/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+
+	"context"
+
+	"github.com/google/wire"
+)
+
+type Message string
+
+// Options
+type Options struct {
+	Messages []Message
+	Writer   io.Writer
+	Reader   io.Reader
+}
+type Greeter struct {
+}
+
+// NewGreeter 使用Options以避免构造函数过长
+func NewGreeter(ctx context.Context, opts *Options) (*Greeter, error) {
+	return nil, nil
+}
+
+// GreeterSet 使用wire.Struct设置Options为provider
+var GreeterSet = wire.NewSet(wire.Struct(new(Options), "*"), NewGreeter)
+
+func main() {
+	fmt.Println(GreeterSet)
+}
